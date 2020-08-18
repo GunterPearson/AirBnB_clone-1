9d30a13fd2e4906555d92ca6eabc099961d32459
root <root@DESKTOP-5KPD6TA.localdomain>
2020-08-17 22:39:32 -0500

    pushing file 2-do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,14 +18,15 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/{}".format(file))
         run("rm -rf /data/web_static/releases/{}/".format(name))
         run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            file, name))
         run("rm /tmp/{}".format(file))
         run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name))
+            "/data/web_static/releases/{}/".format(name, name))
         run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name))
+            format(name))
         return True
     except:
         return False
