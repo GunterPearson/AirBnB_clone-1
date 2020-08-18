0f503c87f73b06d9fac8d84532a06325777eaa5d
root <root@DESKTOP-5KPD6TA.localdomain>
2020-08-17 22:29:17 -0500

    pushing file 2-do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" send archive to servers"""
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+
+env.hosts = ["34.74.82.97", "54.236.20.188"]
+
+
+def do_deploy(archive_path):
+    """ deploy archive in servers"""
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+    try:
+        put(archive_path, "/tmp/{}".format(file))
+        run("rm -rf /data/web_static/releases/{}/".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name))
+        return True
+    except:
+        return False
