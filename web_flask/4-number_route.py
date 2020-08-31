48b7eb6273a1b6afa059dfc14c4354807118baf2
GunterPearson <1715@holbertonschool.com>
2020-08-31 13:28:40 -0500

    pushing files 0-5 and templates

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" 2-c_route """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ for basic / """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ for /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def string_var(text):
+    """ return text """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_var(text="is cool"):
+    """ returns is cool or text """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ return if number """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
